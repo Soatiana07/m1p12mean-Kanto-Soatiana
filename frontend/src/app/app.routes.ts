8aabda0b7522db558c758fef449cc41f108ced55
Soatiana07 <99266931+Soatiana07@users.noreply.github.com>
2025-03-16 13:05:33 +0300

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { MarqueVoitureComponent } from './components/Manager/marque-voiture/marque-voiture.component';
+import { MarqueVoitureComponent } from './demo/marque-voiture/marque-voiture.component';
 export const routes: Routes = [
  { path: 'marqueVoiture', component: MarqueVoitureComponent },
  { path: '', redirectTo: 'articles', pathMatch: 'full' } // Redirectipar défaut

5a7e3cd2e479a2e988462a3448440a1ccbce37d9
Soatiana07 <99266931+Soatiana07@users.noreply.github.com>
2025-04-01 17:01:44 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,17 +7,12 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-// app.use(cors());
-let corsOptions = {
-    origin: ['http://localhost:4200'],
-}
-app.use(cors(corsOptions));
-
-app.use(express.json());
+app.use(cors());
 app.use(authMiddleware);
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Connecte");
+    res.header("Access-Control-Allow-Methods","GET,PUT,POST,DELETE,OPTIONS");
     res.header("Access-Control-Expose-Headers", "X-Connecte");
     next();
 });

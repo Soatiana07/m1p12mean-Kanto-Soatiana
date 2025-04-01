910c27ba1c05f8cd4d8c3703234beafabc3fd108
Soatiana07 <99266931+Soatiana07@users.noreply.github.com>
2025-04-01 17:29:08 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const app = express();
 
 // Middleware
 app.use(cors());
+app.use(express.json());
 app.use(authMiddleware);
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
